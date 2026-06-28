6dd5ce650201879f8307c1a729472a1510e1302d
mgaither32 <hello@beyondthemomentadoptionstudio.com>
2026-06-28 14:34:19 -0400

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="application-name" content="Ask the Adoptee" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Ask the Adoptee" />
         <meta name="theme-color" content="#2C1810" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </Head>
       <body>
         <Main />
